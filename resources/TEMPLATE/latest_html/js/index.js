393c90535bcf1a17dec0136efcfcc093bde1d124
Ivan Kubik <kubik.ivan92@gmail.com>
2020-05-10 07:40:47 +0300

    index.html теперь только нужны скрипты

--- resources/TEMPLATE/latest_html/js/index.js
+++ resources/TEMPLATE/latest_html/js/index.js
@@ -21,3 +21,31 @@ $(document).on('click', function(e) {
     }
     e.stopPropagation();
 });
+
+$(function() {
+    $(document).on("click", ".mobile_menu_container span", function(e) {
+        e.preventDefault();
+        $(this).siblings("ul").addClass("loaded");
+    });
+    $(document).on("click", ".mobile_menu_container .back", function(e) {
+        e.preventDefault();
+        $(this).parent().parent().removeClass("loaded");
+    });
+    $(document).on("click", ".mobile_menu", function(e) {
+        e.preventDefault();
+        $(".mobile_menu_container").addClass("loaded");
+        $(".mobile_menu_overlay").fadeIn();
+        $("body").addClass("no-overlay");
+    });
+    $(document).on("click", ".mobile_menu_overlay", function(e) {
+        $(".mobile_menu_container").removeClass("loaded");
+        $("body").removeClass("no-overlay");
+        $(this).fadeOut();
+    });
+    $(document).on("click", ".mobile_menu_close", function(e) {
+        e.preventDefault();
+        $(".mobile_menu_container").removeClass("loaded");
+        $(".mobile_menu_overlay").fadeOut();;
+        $("body").removeClass("no-overlay");
+    });
+});
