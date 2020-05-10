5732f02eacbc6e562e4ba96f8a161c3234429593
Ivan Kubik <kubik.ivan92@gmail.com>
2020-05-10 08:09:32 +0300

    Убрал ошибку при клике на ссылки

--- resources/TEMPLATE/latest_html/js/main.js
+++ resources/TEMPLATE/latest_html/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-$("body").on('click', '[href*="#"]', function(e){
+$("body").on('click', '.scroll-to', function(e){
   var fixed_offset = 100;
   $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
   e.preventDefault();
