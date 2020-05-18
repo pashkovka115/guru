f7511d8d371abb5e4ff8d91eea724e9290671ba5
Ivan Kubik <kubik.ivan92@gmail.com>
2020-05-18 15:10:30 +0300

    Правки небольшие

--- resources/views/pages/journal/show.blade.php
+++ resources/views/pages/journal/show.blade.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-lg-12">
             <img src="http://a0437878.xsph.ru/assets/site/images/home_bg_new.jpg" alt="" class="img-fluid img-journal">
-            <p>{{ $post->content }}</p>
+            <p class="text_normal">{{ $post->content }}</p>
             </div>
         </div>
     </div>
