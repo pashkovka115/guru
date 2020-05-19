e68802a87fd97c2e8924deb7add238015173aed8
Ivan Kubik <kubik.ivan92@gmail.com>
2020-05-19 03:38:36 +0300

    Новый пакет правок

--- resources/views/parts/filter_panel.blade.php
+++ resources/views/parts/filter_panel.blade.php
@@ -4,21 +4,21 @@
             <div class="col-lg-12">
                 <ul class="filter-category">
                     <li>
-                        <a href="#">Выбрать категорию</a>
+                        <span class="category-result">Выбрать категорию</span>
                         <div class="subcategory">
                             <div class="category_filter_block">
                                 <div class="category_group_filter">
                                     <label>
-                                        <input type="checkbox" name="cat1" value="cat1">
+                                        <input type="checkbox" name="cat1" value="Категория 1">
                                         <span>Категория 1</span>
                                     </label>
                                     <label>
-                                        <input type="checkbox" name="cat1" value="cat1">
-                                        <span>Категория 1</span>
+                                        <input type="checkbox" name="cat1" value="Категория 2">
+                                        <span>Категория 2</span>
                                     </label>
                                     <label>
-                                        <input type="checkbox" name="cat1" value="cat1">
-                                        <span>Категория 1</span>
+                                        <input type="checkbox" name="cat1" value="Категория 3">
+                                        <span>Категория 3</span>
                                     </label>
                                     <label>
                                         <input type="checkbox" name="cat1" value="cat1">
@@ -44,7 +44,7 @@
                         </div>
                     </li>
                     <li>
-                        <a href="#">Выбрать место</a>
+                        <span class="place-result">Выбрать место</span>
                         <div class="subcategory">
                             <div class="category_filter_block">
                                 <div class="category_group_filter">
@@ -84,22 +84,32 @@
                         </div>
                     </li>
                     <li>
-                        <a href="#" id="demo" style="display: block;">Выбрать даты</a>
+                        <span id="demo" style="display: block;">Выбрать даты</span>
                     </li>
                     <li>
-                        <a href="#">Продолжительность</a>
+                        <span class="range-result">Продолжительность</span>
                         <div class="subcategory">
-                            <div class="category_filter_block">
-                                <div class="category_group_filter">
-
-                                </div>
+                            <div class="filter_range">
+                                <div class="range-result">Продолжительность</div>
+                                <div class="range-line"></div>
                             </div>
                             <div class="category_button_filter">
                                 <button type="submit">Применить</button>
                             </div>
                         </div>
                     </li>
-                    <li><a href="#">Цена</a></li>
+                    <li>
+                        <span class="range-price-result">Цена</span>
+                        <div class="subcategory">
+                            <div class="filter_range">
+                                <div class="range-price-result">Цена</div>
+                                <div class="range-price"></div>
+                            </div>
+                            <div class="category_button_filter">
+                                <button type="submit">Применить</button>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
