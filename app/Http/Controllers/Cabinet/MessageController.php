bb83b9366c35819818a3101f6567fe4abf26d8ce
Sergey Smirnov <pariss8@mail.ru>
2021-04-18 11:11:17 +0300

    Исправил отправку вопроса автору

--- app/Http/Controllers/Cabinet/MessageController.php
+++ app/Http/Controllers/Cabinet/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Cabinet;
 
 use App\Http\Controllers\Controller;
+use App\Mail\QuestionFromUser;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Modules\Admin\Models\Message;
@@ -20,7 +21,7 @@ class MessageController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
+        $data = $request->validate([
             'leader'=> 'required|numeric',
             'name'=> 'required|regex:/[\w\s\-]*/i',
             'email'=> 'required|email',
@@ -28,32 +29,9 @@ class MessageController extends Controller
             'message'=> 'required|regex:/[\w\s\-\,\.]*/i',
         ]);
 
-        // TODO: для будущей реализации
-        /*Message::create([
-            'leader_id'=> $request->input('leader'),
-            'name'=> $request->input('name'),
-            'email'=> $request->input('email'),
-            'phone'=> $request->input('phone'),
-            'message'=> $request->input('message'),
-        ]);*/
-
-        $template = view('email.question', ['data' => $request->all()])->render();
-
         $user = User::where('id', (int)$request->input('leader'))->firstOrFail();
 
-        try {
-            \Mail::raw('Задан вопрос с сайта '. env('APP_NAME'), function ($message) use ($user, $template){
-                $message->from(env('MAIL_USERNAME'));
-                $message->to($user->email);
-                $message->setContentType('text/html');
-                $message->subject($template);
-            });
-        }catch (\Swift_TransportException $exception){
-            return redirect()->back()->withErrors('Неполадки сети. Позже попробуйте ещё раз.');
-        }catch (\Exception $exception){
-            return redirect()->back()->withErrors('ООПС..! Непредвиденная ошибка.');
-        }
-
+        \Mail::to($user->email)->send(new QuestionFromUser($data));
 
         session()->flash('message', 'Отправлено');
         return redirect()->back();
